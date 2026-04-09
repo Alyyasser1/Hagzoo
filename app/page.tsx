07938c326d90574f9ad8c88ef82c9ca6e4226ee1
Aly Yasser <alyyasser728@gmail.com>
2026-04-09 05:24:44 +0200

    implemented landing page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
-export default function Landing() {
-  return;
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import "./page.css";
+import AuthCard from "@/components/auth/AuthCard";
+import { Suspense } from "react";
+import AuthContainer from "@/components/auth/AutheContainer";
+export default async function Landing() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user) redirect("/home");
+  return (
+    <div className="landing-page">
+      <div className="landing-logo-mark">
+        <div className="landing-H">H</div>
+        <div className="landing-dot1"></div>
+        <div className="landing-dot2"></div>
+      </div>
+      <div className="landing-name">Hagzoo</div>
+      <div className="landing-tagline">
+        Find players. Join games. Earn rewards.
+      </div>
+      <AuthCard>
+        <Suspense fallback={null}>
+          <AuthContainer></AuthContainer>
+        </Suspense>
+      </AuthCard>
+    </div>
+  );
 }

aa3f1b046354e2fad80ee3527d7de7c68b19c0b7
Aly Yasser <alyyasser728@gmail.com>
2026-04-11 04:01:30 +0200

    Implemented shared layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,26 @@
-export default function HomeLayout({
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
+import { createClient } from "@/lib/supabase/server";
+import { getUserById } from "@/services/userService";
+import { redirect } from "next/navigation";
+
+export default async function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) redirect("/");
+  const { data: userData } = await getUserById(user.id);
+  if (!userData) redirect("/");
+  return (
+    <>
+      <Navbar user={userData} />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }

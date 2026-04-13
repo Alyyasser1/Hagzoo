68a92aade1037b104ed2d7103d39e75e216b66fb
Aly Yasser <alyyasser728@gmail.com>
2026-04-13 18:28:19 +0200

    Updated permission for Image component usage

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
+const supabaseHostname = process.env.NEXT_PUBLIC_SUPABASE_URL 
+  ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname 
+  : '';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:supabaseHostname, // Replace with your actual Supabase URL
+      },
+    ],
+  },
   experimental: {
   caseSensitiveRoutes: true
 }

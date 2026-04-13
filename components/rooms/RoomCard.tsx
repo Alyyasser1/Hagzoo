59f33e9576d54d1ed20f4f57f87258b16dd6c71e
Aly Yasser <alyyasser728@gmail.com>
2026-04-13 18:29:17 +0200

    implemented RoomCard component

---
+++ components/rooms/RoomCard.tsx
@@ -0,0 +1,67 @@
+import { RoomWithOwner } from "@/types/data";
+import "./RoomCard.css";
+import Image from "next/image";
+import { getInitials } from "@/utils/userUtils";
+
+interface RoomCardProps {
+  room: RoomWithOwner;
+}
+const RoomCard = ({ room }: RoomCardProps) => {
+  return (
+    <div className="room-card">
+      <div className="room-card-top">
+        <div className="sport-tag">
+          <div className="sport-icon">
+            {room.sport === "football"
+              ? "⚽"
+              : room.sport === "tennis"
+                ? "🥎"
+                : room.sport === "padel"
+                  ? "🎾"
+                  : "⚽🥅"}
+          </div>
+          <div className="sport-name">{room.sport}</div>
+        </div>
+        <div
+          className={`status-dot ${room.status === "open" ? "green" : "red"}`}
+        >
+          {room.status}
+        </div>
+      </div>
+      <div className="room-name">{room.name}</div>
+      <div className="room-meta">
+        <div className="room-meta-row">📍 {room.location}</div>
+        <div className="room-meta-row">
+          {room.scheduled_date}—{room.scheduled_time}
+        </div>
+      </div>
+      <hr className="room-divider"></hr>
+      <div className="room-footer">
+        <div className="owner">
+          <div className="owner-avatar">
+            {room.users.avatar_url ? (
+              <Image
+                src={room.users.avatar_url}
+                alt={room.users.username}
+                className="avatar-img"
+              ></Image>
+            ) : (
+              <div className="avatar-initials">
+                {getInitials(room.users.username)}
+              </div>
+            )}
+          </div>
+          <div className="owner-name">{room.users.username}</div>
+        </div>
+        <div className="room-right">
+          <div className="players-count">
+            {room.current_players}/{room.max_players}
+          </div>
+          <div className={`level-badge ${room.level}`}>{room.level}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomCard;

6f62dcd7c9a444b89d63ac8806750cbd661198b5
Aly Yasser <alyyasser728@gmail.com>
2026-04-16 03:19:31 +0200

    implement a logout function

--- utils/userUtils.ts
+++ utils/userUtils.ts
@@ -1,5 +1,16 @@
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { createClient } from "@/lib/supabase/client";
+
 export const getInitials = (name: string) => {
+  if (!name) return "??";
   return name.includes(" ")
     ? name.split(" ").map(n => n[0]).join("").slice(0, 2).toUpperCase()
     : name.slice(0, 2).toUpperCase()
 }
+
+export const logout = async (router: AppRouterInstance) => {
+  const supabase = createClient();
+  await supabase.auth.signOut();
+  router.push("/");
+  router.refresh();
+};

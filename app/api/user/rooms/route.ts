58b4f2ce7c14423318e50bcd8073acacfaf3c526
Aly Yasser <alyyasser728@gmail.com>
2026-04-17 04:15:58 +0200

    Fixed query to match the profile demands

--- app/api/user/rooms/route.ts
+++ app/api/user/rooms/route.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
-import { getUserRooms } from "@/services/userService";
+import { getUserWithRooms } from "@/services/userService";
 // calls getUserRooms
 export async function GET(request: Request) {
     const supabase = await createClient();
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
       } = await supabase.auth.getUser();
       if (!user) return Response.json({ error: "Unauthorized" }, { status: 401 });
       const id = user.id
-      const {data,error} =await getUserRooms(id)
+      const {data,error} =await getUserWithRooms(id)
       if(error) return  Response.json({ error }, { status: 500 });
       return Response.json({ data }, { status: 200 });
     }
--- services/userService.ts
+++ services/userService.ts
@@ -1,4 +1,4 @@
-import { User, RoomPlayer, Room } from "../types/data";
+import { User, RoomPlayer, Room, UserWithRooms, RoomWithOwner } from "../types/data";
 import { createClient } from "../lib/supabase/server";
 import { PostgrestError } from "@supabase/supabase-js";
 export const getUserById = async (
@@ -39,14 +39,51 @@ export const updatePlayerStatus = async (
     .single<RoomPlayer>();
   return { data, error };
 };
-export const getUserRooms = async (
-  id: string,
-): Promise<{ data: Room[] | null; error: PostgrestError | null }> => {
+
+interface RoomQueryResponse extends Room {
+  users: Pick<User, "avatar_url" | "username"> | null;
+}
+ 
+interface UserQueryResponse extends User {
+  room_players: {
+    rooms: RoomQueryResponse | null;
+  }[];
+}
+ 
+export const getUserWithRooms = async (
+  userId: string,
+): Promise<{ data: UserWithRooms | null; error: PostgrestError | null }> => {
   const supabase = await createClient();
+ 
   const { data, error } = await supabase
-    .from("room_players")
-    .select("rooms!inner(*)")
-    .eq("user_id", id);
-  const rooms = data?.map((item) => item.rooms) as unknown as Room[];
-  return { data: rooms ?? null, error };
+    .from("users")
+    .select(`
+      *,
+      room_players (
+        rooms (
+          *,
+          users (
+            username,
+            avatar_url
+          )
+        )
+      )
+    `)
+    .eq("id", userId)
+    .single();
+ 
+  if (error || !data) return { data: null, error };
+ 
+  const rawData = data as unknown as UserQueryResponse;
+ 
+  const rooms: RoomWithOwner[] = rawData.room_players
+    ?.map((item) => item.rooms)
+    .filter((room): room is RoomQueryResponse => room !== null);
+ 
+  const userWithRooms: UserWithRooms = {
+    ...rawData,
+    rooms,
+  };
+ 
+  return { data: userWithRooms, error: null };
 };

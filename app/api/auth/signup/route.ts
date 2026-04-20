03c03fa0ab4a263e1d2cb2f536f962205cd5fa92
Aly Yasser <alyyasser728@gmail.com>
2026-04-20 14:07:23 +0200

    Fix account exsit message

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
     const supabase = await createClient();
     const body = await request.json();
     const { username,email,tel,birthDate,level, password } = body;
-    const { error } = await supabase.auth.signUp({email,password,options:{
+    const { data,error } = await supabase.auth.signUp({email,password,options:{
         data:{
             username,
             phone:tel,
@@ -18,8 +18,14 @@ export async function POST(request: Request) {
       return NextResponse.json(
         { error: error.message }, 
         { status: 400 }
-      );
+      ); 
     }
+    if (data.user && data.user.identities && data.user.identities.length === 0) {
+  return NextResponse.json(
+    { error: "Account already exists. Try to login." },
+    { status: 400 }
+  );
+  }
     return NextResponse.json({ message: "signup successful" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
